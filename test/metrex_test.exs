053f2cbbee5ca542fed138110eef4da985f8ab01
Mustafa TURAN <mustafaturan.net@gmail.com>
2016-10-07 03:21:19 +0100

    Added start child for counter and meter.

--- test/metrex_test.exs
+++ test/metrex_test.exs
@@ -9,4 +9,14 @@ defmodule MetrexTest do
   test "sample meter metric is alive?" do
     refute is_nil(GenServer.whereis(:"meter.m_sample"))
   end
+
+  test "sample counter child metric is alive?" do
+    Metrex.start_counter("counter_child")
+    refute is_nil(GenServer.whereis(:"counter.counter_child"))
+  end
+
+  test "sample meter child metric is alive?" do
+    Metrex.start_meter("meter_child")
+    refute is_nil(GenServer.whereis(:"meter.meter_child"))
+  end
 end
